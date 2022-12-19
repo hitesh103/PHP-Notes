f5c275e20e8d70dfedf2e1814e6a1590cfa7b8a3
Hitesh Prajapati <hp5741609@gmail.com>
2022-12-19 18:08:33 +0530

    Add files via upload

---
+++ File_inclusion/fileInclusion.php
@@ -0,0 +1,34 @@
+<?php
+
+// inclusion in php
+
+// include()
+// require()
+// require_once
+// include_once
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<?php
+include("header.php");
+?>
+<body>
+    <h1>About</h1>
+    <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque a, soluta necessitatibus accusamus eos rerum corrupti quo perspiciatis quam hic commodi consectetur, ipsam deleniti quaerat quos reiciendis, et iste at dicta? Accusantium iusto quam error hic itaque cum, voluptatibus, quis quas, minima labore quos eveniet aut! Facilis nisi aliquid officiis!</div>
+   
+</body>
+<?php
+include("footer.php");
+?>
+
+</html>
+
+// if we use require() to include it is same as include but it stops exectusion after a error but include doesn't
